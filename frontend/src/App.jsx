852860a1d13e9934046dc90f0ebe2c67c59c67cb
felpscybernet <filipesantosaraujo34@gmail.com>
2025-11-11 02:16:03 -0300

    Feature: Inicializa app React com Vite na pasta /frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+// frontend/src/App.jsx
+
+function App() {
+  return (
+    <div>
+      <h1>Olá, este é o novo Frontend React!</h1>
+      <p>Deploy funcionando. O próximo passo é migrar os componentes.</p>
+    </div>
+  )
+}
+
+export default App
